8196995c8634ee9cfe97c65cd12b2a822ed75ce9
lxdv <ledmov@outlook.com>
2025-10-20 18:14:33 +0200

    Add .gitignore and remove build folder from repo

--- build/CMakeFiles/ChessAI.dir/src/BotBasic.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ChessAI.dir/src/BotBasic.cpp.o: \
- /home/tiramisu/Projects/ChessAI/src/BotBasic.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/ChessAI.dir/src/Move.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ChessAI.dir/src/Move.cpp.o: \
- /home/tiramisu/Projects/ChessAI/src/Move.cpp /usr/include/stdc-predef.h
